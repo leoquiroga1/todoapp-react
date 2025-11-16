db29f2118e7fbaf11dbd8d2b399dc06466160d45
Leonardo Quiroga <leoquiroga1@hotmail.com>
2025-11-16 09:25:36 -0500

    corrigiendo llaves

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHzaX5ofpOBYLHU28Ke55Db2_NLh1CDLY",
-  authDomain: "todoapp-react-project-64516.firebaseapp.com",
-  projectId: "todoapp-react-project-64516",
-  storageBucket: "todoapp-react-project-64516.appspot.com",
-  messagingSenderId: "948126970498",
-  appId: "1:948126970498:web:91329105eb8b57f5613a9b",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
